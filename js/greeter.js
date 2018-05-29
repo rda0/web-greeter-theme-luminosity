1a7f0e820f19e0e456948b0b4d5f9c7d12a38735
Sven Mäder <maeder@phys.ethz.ch>
2018-05-29 13:35:30 +0200

    Fix fade callback

--- js/greeter.js
+++ js/greeter.js
@@ -412,7 +412,8 @@ $(document).ready(function () {
     $('.content').css({
       marginLeft: '-450px'
     });
-    $('#actionsArea').fadeOut(2000, function(){$('#backArea').fadeIn(2000);});
+    $('#actionsArea').fadeOut(2000);
+    //, function(){$('#backArea').fadeIn(2000);}
     $('#session-list .selected').html(e.target.getAttribute('data-session'));
   }
   window.authenticate = function (e, username) {
