05f4aec5763457ef179a1ab6c38ae0d1cb6e36cf
Sven Mäder <maeder@phys.ethz.ch>
2018-06-28 16:22:27 +0200

    Rename project

--- js/greeter.js
+++ js/greeter.js
@@ -4,14 +4,14 @@
  *
  * greeter.js
  *
- * This file is part of web-greeter-theme-phys
+ * This file is part of web-greeter-theme-luminosity
  *
- * web-greeter-theme-phys is free software: you can redistribute it and/or modify
+ * web-greeter-theme-luminosity is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or any later version.
  *
- * web-greeter-theme-phys is distributed in the hope that it will be useful,
+ * web-greeter-theme-luminosity is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
