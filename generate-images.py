d0ca5e5c97a0c553b263574eddbf1c7526bfa4fc
Sven Mäder <maeder@phys.ethz.ch>
2022-04-05 23:02:11 +0200

    Load background_images_dir from config, fix #1

--- generate-images.py
+++ generate-images.py
@@ -5,20 +5,38 @@ import json
 import pprint
 import subprocess
 import collections
+from ruamel.yaml import YAML
 
 FORMATS = [ '.jpg', '.jpeg', '.png' ]
-PATH_WALL = [ 'img/wallpapers', '/usr/share/backgrounds' ]
 PATH_THUMB = 'img/thumbs'
+PATH_CONFIG = '/etc/lightdm/web-greeter.yml'
 SIZE = '100x56'
+
+yaml = YAML(typ="rt")
+paths = [ 'img/wallpapers' ]
+
+def get_config(fpath):
+    """Read yaml from file"""
+    with open(fpath) as f:
+        try:
+            return yaml.load(f)
+        except Exception as e:
+            print(f"{Y}Warning: could not parse yaml file: {fpath}{A}")
+            print(f"{Y}Exception: {e}{A}")
+            return None
+
+config = get_config(PATH_CONFIG)
+if config and 'branding' in config and 'background_images_dir' in config['branding']:
+    path = config['branding']['background_images_dir']
+    if path:
+        paths.append(path)
 
 scriptpath = os.path.realpath(__file__)
 dirpath = os.path.dirname(scriptpath)
 abspath = os.path.abspath(dirpath)
 os.chdir(abspath)
 
-images = list()
-
-for path in PATH_WALL:
+for path in paths:
     for root, directories, filenames in os.walk(path):
         for filename in filenames:
             filepath = os.path.join(root, filename)
